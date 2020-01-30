6dbc92289d864cc64bbcaceaec49d0bcc422a80d
Igor Smirnov <piter3343@gmail.com>
2020-01-30 22:48:20 +0300

    More javadocs for the God of javadocs!

--- HA01_CLI/src/main/kotlin/ru/spbau/smirnov/cli/preprocessing/parsing/OperationVisitor.kt
+++ HA01_CLI/src/main/kotlin/ru/spbau/smirnov/cli/preprocessing/parsing/OperationVisitor.kt
@@ -5,11 +5,17 @@ import ru.spbau.smirnov.CLIParser
 import ru.spbau.smirnov.cli.executor.ExecutableFactory
 import ru.spbau.smirnov.cli.executor.commands.Executable
 
+/**
+ * Visitor for grammar from `CLI.g4`.
+ *
+ * Visits command in their execution order and adds them to `commands` list
+ */
 class OperationVisitor(
     private val commands: MutableList<Executable>,
     private val executableFactory: ExecutableFactory
 ): CLIBaseVisitor<String?>() {
 
+    /** Visits assignment statement and creates `Assignment` object */
     override fun visitAssignment(ctx: CLIParser.AssignmentContext): String? {
         val variableName = ctx.variable.text
         val value = ctx.value.accept(this)!!
@@ -20,6 +26,7 @@ class OperationVisitor(
         return null
     }
 
+    /** Visits set of command in their execution order */
     override fun visitSetOfCommands(ctx: CLIParser.SetOfCommandsContext): String? {
         println("Visited ${ctx.text}")
         for (command in ctx.commands) {
@@ -29,6 +36,7 @@ class OperationVisitor(
         return null
     }
 
+    /** Creates command */
     override fun visitCommand(ctx: CLIParser.CommandContext): String? {
         val arguments = mutableListOf<String>()
         for (argument in ctx.args) {
@@ -42,14 +50,17 @@ class OperationVisitor(
         return null
     }
 
+    /** Deletes quotes from double quoted token */
     override fun visitDoubleQuote(ctx: CLIParser.DoubleQuoteContext): String {
         return ctx.text.substring(1 until ctx.text.lastIndex)
     }
 
+    /** Deletes quotes from single quoted token */
     override fun visitSingleQuote(ctx: CLIParser.SingleQuoteContext): String {
         return ctx.text.substring(1 until ctx.text.lastIndex)
     }
 
+    /** Returns token's value */
     override fun visitUnquote(ctx: CLIParser.UnquoteContext): String {
         return ctx.text
     }
--- HA01_CLI/src/main/kotlin/ru/spbau/smirnov/cli/preprocessing/substitution/Substitute.kt
+++ HA01_CLI/src/main/kotlin/ru/spbau/smirnov/cli/preprocessing/substitution/Substitute.kt
@@ -2,7 +2,18 @@ package ru.spbau.smirnov.cli.preprocessing.substitution
 
 import ru.spbau.smirnov.cli.Environment
 
+/**
+ * Class for substitution variable names with their values.
+ *
+ * Variable names will be substituted, if they are not inside weak quotes
+ */
 class Substitute(private val environment: Environment) {
+    /**
+     * Performs substitution.
+     *
+     * @throws SubstitutionParserException if some error occurred while parsing
+     * (i.e. not closed quotes)
+     */
     fun doSubstitution(input: String): String {
         var stateMachine: StateMachineVertex = UsualState()
         val tokens = mutableListOf<Token>()
